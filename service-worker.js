3096af4b5bb65474103e4bae8bb5e986971eff69
roseycalamari <rosaruben7@gmail.com>
2026-05-07 15:48:34 +0100

    m

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
    every installed PWA will purge old caches and refresh on next open.
    ========================================================================== */
 
-const CACHE_VERSION = 'v7';
+const CACHE_VERSION = 'v8';
 const RUNTIME_CACHE  = 'niterun-runtime-' + CACHE_VERSION;
 const STATIC_CACHE   = 'niterun-static-'  + CACHE_VERSION;
 
